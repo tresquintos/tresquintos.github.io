f45850ca156c477550fd0a90005f0d839c211bbd
Kenneth Bunker <kabunker@gmail.com>
2020-03-16 15:50:14 +0100

    Update konami.js

--- js/konami.js
+++ js/konami.js
@@ -38,8 +38,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 function activateCheats() {
-  window.location.href("http://www.w3schools.com");
-    //document.body.style.backgroundImage = "url('https://supermariobros.io/')";
+  window.location.href("http://www.supermariobros.io");
     //document.body.style.backgroundImage = "url('images/cheatBackground.png')";
 
 

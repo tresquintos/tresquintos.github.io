90bb4d5fb012d5b289f9f8e2b398c62cb39d980a
Kenneth Bunker <kabunker@gmail.com>
2021-08-07 19:41:21 +0200

    d

---
+++ assets/donationrequest/README.markdown
@@ -0,0 +1,5 @@
+# Donation Request Pop Up
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/shaneapen/pen/vYLwbXK](https://codepen.io/shaneapen/pen/vYLwbXK).
+
+
---
+++ assets/donationrequest/dist/script.js
@@ -0,0 +1,21 @@
+var closeButton = document.getElementById('closeButton');
+var modal = document.getElementById("donationBoxModel");
+
+closeButton.addEventListener('click', function(){
+    modal.parentElement.style.display = modal.style.display = "none";
+    showQR(false);
+})
+
+function showModal(){
+       modal.style.display = modal.parentElement.style.display = "block";
+}
+
+function showQR(showQR) {
+    if(showQR){
+        document.getElementById("qrCodeContainer").classList.remove("hidden");
+         document.getElementsByClassName("profile")[0].classList.add("hidden");
+    }else{
+        document.getElementById("qrCodeContainer").classList.add("hidden");
+        document.getElementsByClassName("profile")[0].classList.remove("hidden");
+    }
+}

26988ff7aeea0f7e89acb5a10978a088513d23fc
Kenneth Bunker <kabunker@gmail.com>
2021-08-07 19:48:08 +0200

    ll

--- assets/donationrequest/dist/script.js
+++ assets/donationrequest/dist/script.js
@@ -1,21 +1,50 @@
-var closeButton = document.getElementById('closeButton');
-var modal = document.getElementById("donationBoxModel");
+$(document).ready(function(){
 
-closeButton.addEventListener('click', function(){
-    modal.parentElement.style.display = modal.style.display = "none";
-    showQR(false);
-})
+  $('#searchbar').focus();
 
-function showModal(){
-       modal.style.display = modal.parentElement.style.display = "block";
-}
+  $('#donate-buttons').on('click', '.btn-blue', function(e) {
+    e.preventDefault();
+    $('.active').removeClass('active');
+    $('#other-input').hide().siblings('#other').show();
+    $(this).filter('.btn-blue').addClass("active");
+    var value = $(this).data('impact');
+    $(this).closest('div').find('p').text("" + value);
+    $('#other-input').find('input').val('');
+  });
 
-function showQR(showQR) {
-    if(showQR){
-        document.getElementById("qrCodeContainer").classList.remove("hidden");
-         document.getElementsByClassName("profile")[0].classList.add("hidden");
-    }else{
-        document.getElementById("qrCodeContainer").classList.add("hidden");
-        document.getElementsByClassName("profile")[0].classList.remove("hidden");
+  $('.btn-green').on('click', function() {
+    var dollar;
+    var input = $('#other-input').find('input').val();
+    if ( !input ) {
+      dollar = $('.active').data('dollars');
+     } else if ( $.trim(input) === '' || isNaN(input)) {
+      // empty space leaves value = 'undefined'.
+      // Have to fix $.trim(input) == '' above so that it works.
+      console.log('Yes');
+      dollar = "Please enter a number.";
+    } else {
+      dollar = input;
     }
-}
+    $('#price').text(""+dollar);
+  });
+
+  $('#other').on('click', function(e) {
+    e.preventDefault();
+    var buttons = $(this).parent('#donate-buttons');
+    buttons.find('.active').removeClass('active');
+    var other = $(this).hide().siblings('#other-input');
+    other.show();
+    other.find('input').focus();
+    var pText = buttons.siblings('p');
+    pText.text("Thank you!");
+    var oValue = other.find('input');
+    oValue.keyup(function() {
+      if ( oValue.val() > 50 ) {
+        pText.text("Thank you!" + " You\'re donation covers housing and counseling services for " + oValue.val()/25 + " people.");
+      } else {
+        pText.text("Thank you!");
+      }
+    });
+  });
+
+});
